b17bb8245ba31b6e4e705a427f8ebc87400ec3ca
Stanislav Lukyanchikov <lukyanchikov.si@gmail.com>
2022-12-13 18:19:21 +0500

    Решение задачи 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Задача 1
+        checkLeapYearAndPrint(2012);
+        checkLeapYearAndPrint(2022);
+
+    }
+
+    public static void checkLeapYearAndPrint(int year){
+
+        boolean leap = false;
+
+        if (year % 4 == 0) {
+
+            if (year % 100 == 0) {
+
+                if (year % 400 == 0)
+                    leap = true;
+
+            } else {
+                leap = true;
+            }
+        }
+
+        if (leap){
+            System.out.println(year + " — високосный год");
+        } else {
+            System.out.println(year + " — не високосный год");
+        }
+
     }
 }
