ba1a34793d7c5e7bab43d34f56e7a4f50ad1f937
Stanislav Lukyanchikov <lukyanchikov.si@gmail.com>
2022-12-13 18:32:31 +0500

    Решение задачи 2

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,19 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
 
         // Задача 1
         checkLeapYearAndPrint(2012);
         checkLeapYearAndPrint(2022);
+
+        // Разделитель
+        System.out.println();
+
+        // Задача 2
+        checkDevice(0, 2015);
+        checkDevice(1, 2022);
+        checkDevice(3, 2022); // Ошибка
 
     }
 
@@ -28,6 +38,27 @@ public class Main {
         } else {
             System.out.println(year + " — не високосный год");
         }
+    }
 
+    public static void checkDevice(int os, int year){
+
+        int currentYear = LocalDate.now().getYear();
+
+        String osName = switch (os) {
+            case 0 -> "iOS";
+            case 1 -> "Android";
+            default -> "Unknown";
+        };
+
+        if (osName.equals("Unknown")){
+            System.err.println("Неверный тип ОС");
+            return;
+        }
+
+        if (year < currentYear) {
+            System.out.println("Установите облегченную версию приложения для " + osName + " по ссылке");
+        } else {
+            System.out.println("Установите приложение для " + osName + " по ссылке");
+        }
     }
 }
